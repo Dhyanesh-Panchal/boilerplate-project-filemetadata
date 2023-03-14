1ebcaf5b6e0a6baaaed10993ed1798db917bd945
Dhyanesh Panchal <97329341+Dhyanesh-Panchal@users.noreply.github.com>
2023-03-14 18:54:54 +0000

    All tests passed for file metadata microservice.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload=multer({dest: 'uploads/'})
 require('dotenv').config()
 
 var app = express();
@@ -10,6 +12,18 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+// upload is a multer function which here we used as middleware, so it add file to req.file.
+
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  //here req.file is the file data added by middleware.
+  let resObj={
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size 
+  }
+  res.json(resObj);
+})
 
 
 
